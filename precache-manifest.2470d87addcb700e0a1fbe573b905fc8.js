b632f4619e98a3f43ae77c6ac6d7ac859e28095a
Michael Zaleski <me@mikezaleski.com>
2020-07-07 13:20:31 -0400

    Updates

---
+++ precache-manifest.2470d87addcb700e0a1fbe573b905fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213ed3e6ec3408e4e216a81090cfaa8d",
+    "url": "/monsters-rebuild/index.html"
+  },
+  {
+    "revision": "bd1c67f84d91cb7f58da",
+    "url": "/monsters-rebuild/static/css/main.a4a76671.chunk.css"
+  },
+  {
+    "revision": "42bd49850ec002c4f95e",
+    "url": "/monsters-rebuild/static/js/2.13ff61e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rebuild/static/js/2.13ff61e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1c67f84d91cb7f58da",
+    "url": "/monsters-rebuild/static/js/main.9358eed8.chunk.js"
+  },
+  {
+    "revision": "75e943e9ea3a3864100a",
+    "url": "/monsters-rebuild/static/js/runtime-main.cb0cf31c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rebuild/static/media/logo.5d5d9eef.svg"
+  }
+]);
